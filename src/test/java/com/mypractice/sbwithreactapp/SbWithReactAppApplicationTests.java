57099dac4ee32b20c24fef71659cf16389a37944
Nasruddinkhan <nasruddinkhan44@gmail.com>
2021-12-19 10:08:17 +0530

    update expense app

---
+++ src/test/java/com/mypractice/sbwithreactapp/SbWithReactAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypractice.sbwithreactapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbWithReactAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
