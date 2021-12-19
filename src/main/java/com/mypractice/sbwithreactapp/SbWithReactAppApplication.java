efcc44929a2bc9db2f6c0ece012890659dab703c
Nasruddinkhan <nasruddinkhan44@gmail.com>
2021-12-19 10:08:17 +0530

    setup the project springboot with react

---
+++ src/main/java/com/mypractice/sbwithreactapp/SbWithReactAppApplication.java
@@ -0,0 +1,13 @@
+package com.mypractice.sbwithreactapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWithReactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbWithReactAppApplication.class, args);
+	}
+
+}
